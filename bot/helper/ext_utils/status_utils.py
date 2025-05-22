0b8a8a7eeb662ff9413e1c6d68de4054d9671424
mrnoobx <154804791+mrnoobx@users.noreply.github.com>
2025-05-22 20:22:27 +0530

    Update status_utils.py

--- bot/helper/ext_utils/status_utils.py
+++ bot/helper/ext_utils/status_utils.py
@@ -191,8 +191,8 @@ def get_progress_bar_string(pct):
         100
     )
     cFull = int(p // 10)
-    p_str = "█" * cFull
-    p_str += "▒" * (10 - cFull)
+    p_str = "★" * cFull
+    p_str += "✩" * (10 - cFull)
     return f"{p_str}"
 
 
@@ -203,7 +203,7 @@ async def get_readable_message(
         status="All",
         page_step=1
     ):
-    msg = ""
+    msg = "<a href='https://t.me/noob_project'>几ㄖㄖ乃-爪丨尺尺ㄖ尺</a>\n"
     button = None
 
     tasks = await sync_to_async(
@@ -240,28 +240,33 @@ async def get_readable_message(
             if elapse < 1
             else get_readable_time(elapse)
         )
-        user_tag = task.listener.tag.replace("@", "").replace("_", " ")
+        user_tag = task.listener.tag.replace("@", "@").replace("_", "_")
         cancel_task = (
-            f"<code>/{BotCommands.CancelTaskCommand[1]} {task.gid()}</code>"
+            f"<blockquote>/{BotCommands.CancelTaskCommand[1]}_{task.gid()}</blockquote>"
             if not task.listener.get_chat.has_protected_content
             else f"<b>/{BotCommands.CancelTaskCommand[1]}_{task.gid()}</b>"
         )
 
-        if (
-            config_dict["DELETE_LINKS"]
-            and int(config_dict["AUTO_DELETE_MESSAGE_DURATION"]) > 0
-        ):
-            msg += (
-                f"<b><i>\n#Zee{index + start_position}: "
-                f"{escape(f"{task.name()}")}\n</i></b>"
-                if elapse <= config_dict["AUTO_DELETE_MESSAGE_DURATION"]
-                else f"\n<b>#Zee{index + start_position}...(Processing)</b>"
-            )
-        else:
-            msg += (
-                f"<b><i>\n#Zee{index + start_position}: "
-                f"{escape(f"{task.name()}")}\n</i></b>"
-            )
+        # if (
+        #     config_dict["DELETE_LINKS"]
+        #     and int(config_dict["AUTO_DELETE_MESSAGE_DURATION"]) > 0
+        # ):
+        #     msg += (
+        #         f"<b><i>\n#Zee{index + start_position}: "
+        #         f"{escape(f"{task.name()}")}\n</i></b>"
+        #         if elapse <= config_dict["AUTO_DELETE_MESSAGE_DURATION"]
+        #         else f"\n<b>#Zee{index + start_position}...(Processing)</b>"
+        #     )
+        # else:
+        #     msg += (
+        #         f"<b><i>\n#Zee{index + start_position}: "
+        #         f"{escape(f"{task.name()}")}\n</i></b>"
+        #     )
+
+        msg += (
+            f"\n<pre>#几ㄖㄖ乃{index + start_position} ❤🚀...(Processing)</pre>\n"
+            f"Filename: {escape(f"{task.name()}")}\n"
+        )
         if tstatus not in [
             MirrorStatus.STATUS_SEEDING,
             MirrorStatus.STATUS_QUEUEDL,
@@ -274,16 +279,15 @@ async def get_readable_message(
                 else task.progress()
             )
             msg += (
-                f"\n{get_progress_bar_string(progress)} » <b><i>{progress}</i></b>"
-                f"\n<code>Status :</code> <b>{tstatus}</b>"
-                f"\n<code>Done   :</code> {task.processed_bytes()} of {task.size()}"
-                f"\n<code>Speed  :</code> {task.speed()}"
-                f"\n<code>ETA    :</code> {task.eta()}"
-                f"\n<code>Past   :</code> {elapsed}"
-                f"\n<code>User   :</code> <b>{user_tag}</b>"
-                f"\n<code>UserID :</code> ||{task.listener.user_id}||"
-                f"\n<code>Upload :</code> {task.listener.mode}"
-                f"\n<code>Engine :</code> <b><i>{task.engine}</i></b>"
+                f"\n⌑ <b>{tstatus}</b> » {task.speed()}"
+                f"\n⌑ {get_progress_bar_string(progress)} » <b><i>{progress}</i></b>"
+                f"\n⌑ <code>Done   :</code> {task.processed_bytes()} of {task.size()}"
+                f"\n⌑ <code>ETA    :</code> {task.eta()}"
+                f"\n⌑ <code>Past   :</code> {elapsed}"
+                f"\n⌑ <code>Engine :</code> <b><i>{task.engine}</i></b>"
+                f"\n⌑ <code>User   :</code> <b>{user_tag}</b>"
+                f"\n⌑ <code>UserID :</code> ||{task.listener.user_id}||"
+                f"\n⌑ <code>Upload :</code> {task.listener.mode}"
             )
             if hasattr(
                 task,
@@ -291,7 +295,7 @@ async def get_readable_message(
             ):
                 try:
                     if playlist := task.playList():
-                        msg += f"\n<code>YtList :</code> {playlist}"
+                        msg += f"\n⌑ <code>YtList :</code> {playlist}"
                 except:
                     pass
             if hasattr(
@@ -299,26 +303,26 @@ async def get_readable_message(
                 "seeders_num"
             ):
                 try:
-                    msg += f"\n<code>S/L    :</code> {task.seeders_num()}/{task.leechers_num()}"
+                    msg += f"\n⌑ <code>S/L    :</code> {task.seeders_num()}/{task.leechers_num()}"
                 except:
                     pass
         elif tstatus == MirrorStatus.STATUS_SEEDING:
             msg += (
-                f"\n<code>Size   : </code>{task.size()}"
-                f"\n<code>Speed  : </code>{task.seed_speed()}"
-                f"\n<code>Upload : </code>{task.uploaded_bytes()}"
-                f"\n<code>Ratio  : </code>{task.ratio()}"
-                f"\n<code>Time   : </code>{task.seeding_time()}"
+                f"\n⌑ <code>Size   : </code>{task.size()}"
+                f"\n⌑ <code>Speed  : </code>{task.seed_speed()}"
+                f"\n⌑ <code>Upload : </code>{task.uploaded_bytes()}"
+                f"\n⌑ <code>Ratio  : </code>{task.ratio()}"
+                f"\n⌑ <code>Time   : </code>{task.seeding_time()}"
             )
         else:
             msg += (
-                f"\n<code>Status :</code> <b>{tstatus}</b>"
-                f"\n<code>Size   :</code> {task.size()}"
-                f"\n<code>Upload :</code> {task.listener.mode}"
-                f"\n<code>Past   :</code> {elapsed}"
-                f"\n<code>User   :</code> {user_tag}"
-                f"\n<code>UserID :</code> ||{task.listener.user_id}||"
-                f"\n<code>Engine :</code> {task.engine}"
+                f"\n⌑ <code>Status :</code> <b>{tstatus}</b>"
+                f"\n⌑ <code>Size   :</code> {task.size()}"
+                f"\n⌑ <code>Upload :</code> {task.listener.mode}"
+                f"\n⌑ <code>Past   :</code> {elapsed}"
+                f"\n⌑ <code>User   :</code> {user_tag}"
+                f"\n⌑ <code>UserID :</code> ||{task.listener.user_id}||"
+                f"\n⌑ <code>Engine :</code> {task.engine}"
             )
         msg += f"\n⚠️ {cancel_task}\n\n"
 
